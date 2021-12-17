ed209d11da7f2ac448072124ac73837df3b49c70
arha83 <amirrhosseinali@gmail.com>
2021-12-17 21:59:38 +0330

    loading dataset and normalizing

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from tensorflow import keras
 from tensorflow.keras import datasets, layers, models, losses
 import cv2 as cv
 import numpy as np
+
+# loading dataset:
+print('### loading dataset...')
+(trainImages, trainLabels), (testImages, testLables)= datasets.mnist.load_data()
+# normalizing dataset:
+print('### resizing dataset...')
+trainImages, testImages= trainImages/255.0, testImages/255.0
