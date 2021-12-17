d3491d7663c9493a495d81de3b7e516958158918
arha83 <amirrhosseinali@gmail.com>
2021-12-17 22:19:19 +0330

    model build

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import datasets, layers, models, losses
+from tensorflow.keras import datasets, layers, models, losses, applications
 import cv2 as cv
 import numpy as np
 
@@ -11,3 +11,17 @@ print('### loading dataset...')
 # normalizing dataset:
 print('### resizing dataset...')
 trainImages, testImages= trainImages/255.0, testImages/255.0
+
+# building the model:
+print('### building the model...')
+model= models.Sequential()
+model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64, (3,3), activation='relu', input_shape=(28,28,1)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64, (3,3), activation='relu', input_shape=(28,28,1)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10))
+print(model.summary())
