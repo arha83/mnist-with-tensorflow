045240069ada8becfb6c73a32cc3476d207dafdf
arha83 <arha113918@gmail.com>
2021-12-18 22:03:50 +0330

    prediction

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from tensorflow.keras import datasets, layers, models, losses, applications
 import cv2 as cv
 import numpy as np
 
+'''
+physical_devices = tf.config.list_physical_devices('GPU') 
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+'''
+
+classes= [0,1,2,3,4,5,6,7,8,9]
+
 # loading dataset:
 print('### loading dataset...')
 (trainImages, trainLabels), (testImages, testLables)= datasets.mnist.load_data()
@@ -26,9 +33,20 @@ model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(10))
 print(model.summary())
 
-
+model= models.load_model(os.path.join(os.path.dirname(os.path.abspath(__file__)),'myModel'))
 # training:
 print('### training...')
 model.compile(optimizer='adam', loss=losses.SparseCategoricalCrossentropy(from_logits=True),metrics=['accuracy'])
 model.fit(trainImages, trainLabels, epochs=1, validation_data=(testImages, testLables))
 model.save(os.path.dirname(os.path.abspath(__file__))+'\\myModel')
+
+
+# predicting:
+model= models.load_model(os.path.join(os.path.dirname(os.path.abspath(__file__)),'myModel'))
+image= cv.imread(os.path.dirname(os.path.abspath(__file__))+'\\test images\\3.png')
+image= cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+pre= np.expand_dims(image, 0)
+predictions= model.predict([pre])
+maxi= np.where(predictions[0] == np.amax(predictions[0]))[0][0]
+for i in range(10): print(classes[i], ': ', predictions[0][i], sep='')
+print('\nclosest class:',classes[maxi],'\n\n')
